0d8564d3b6bef9b27744c11f9b0d20da77f84381
fluffy29 <hmajzoub12@gmail.com>
2024-05-20 20:34:09 +0200

    style: Optimize structure and remove unnecessary code for the restaurant website

--- cart.php
+++ cart.php
@@ -4,14 +4,12 @@ session_start();
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cart</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
-
 <body>
     <section class="navbar">
         <div class="container">
@@ -56,5 +54,4 @@ session_start();
         </div>
     </section>
 </body>
-
 </html>
